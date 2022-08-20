cf8346a11e10183cd5d62bdaa9c42a96e8f5e316
ViV3K <you@example.com>
2022-08-20 23:25:07 +0530

    first commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div className = "navbar-brand">
+                            Employee Management Application
+                    </div>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
